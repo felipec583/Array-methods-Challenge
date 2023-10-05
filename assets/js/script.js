4eddeea411a4d48846aab8d10607d8106539786b
Felipe Castillo <felipecastillo48@gmail.com>
2023-10-05 17:20:37 -0300

    Second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,26 @@ const completedTasksCount = document.querySelector(".task-done");
 const taskInfoContainer = document.querySelector(".rows");
 const addTaskButton = document.querySelector(".add-btn");
 let counter = 4;
-const taskCounter = document.createElement("span");
+const taskCounter = createElement("span", "task-counter");
 totalTasksCount.append(taskCounter);
-taskCounter.classList.add("task-counter");
-
-const completedTaskCounter = document.createElement("span");
+const completedTaskCounter = createElement("span", "completed-task-counter");
 completedTasksCount.append(completedTaskCounter);
-completedTaskCounter.classList.add("completed-task-counter");
+
+function createElement(ele, eleClass = "") {
+  const elem = document.createElement(`${ele}`);
+  elem.classList.add(`${eleClass}`);
+  return elem;
+}
 
 const tasks = [
-  { id: 1, task: "buy groceries", completed: false },
-  { id: 2, task: "run morning errands", completed: false },
-  { id: 3, task: "walk my dog", completed: false },
+  { id: 1, task: "buy groceries", completed: false, isInEditionMode: false },
+  {
+    id: 2,
+    task: "run morning errands",
+    completed: false,
+    isInEditionMode: false,
+  },
+  { id: 3, task: "walk my dog", completed: false, isInEditionMode: false },
 ];
 const countTasks = () => {
   taskCounter.innerHTML = ` ${tasks.length}`;
@@ -26,13 +34,19 @@ const renderTaskRow = () => {
     const checkInput = task.completed
       ? `<input type="checkbox" checked class="task-check"/>`
       : `<input type="checkbox" class="task-check"/>`;
-    const checkedText = task.completed
-      ? `<p class="task completed">${task.task}</p>`
-      : `<p class="task">${task.task}</p>`;
+    const checkedText =
+      task.completed && !task.isInEditionMode
+        ? `<p class="task completed">${task.task}</p> 
+      <button class="edit">Edit</button>`
+        : `<p class="task">${task.task}</p><button class="edit">Edit</button>`;
+    const editionMode = task.isInEditionMode
+      ? `<input class="new-input" type="text">
+        <button class="edit">Save</button>
+        <button class="cancel-btn">Cancel</button>`
+      : `${checkedText} ${checkInput}`;
     htmlCont += `<div id="${task.id}" class="task-row">
       <span>${task.id}</span> 
-      ${checkedText}
-      ${checkInput}
+      ${editionMode}
       <span class="del-btn">&#x274C;</span>
     </div>`;
   });
@@ -42,24 +56,33 @@ const renderTaskRow = () => {
 renderTaskRow();
 
 function generateId() {
+  if (tasks.length === 0) {
+    counter = 1;
+  }
   const id = counter;
-  counter++;
+  ++counter;
   return id;
 }
 
 addTaskButton.addEventListener("click", (e) => {
   const taskInputValue = taskInput.value;
+  /*  let newId = Math.floor(Math.random() * Date.now());
+  newId = +newId.toString().substring(0, 3); */
+
   const newTask = { id: generateId(), task: taskInputValue, completed: false };
   if (taskInputValue !== "") {
     tasks.push(newTask);
     taskInput.value = "";
+    console.log(tasks);
+    console.log(counter);
+
     renderTaskRow();
   }
 });
 
 const changeTaskStatusAndTaskText = (arr, id, e) => {
   const ind = arr.find((elem) => elem.id === id);
-  const text = e.target.previousElementSibling;
+  const text = e.target.previousElementSibling.previousElementSibling;
   if (ind && e.target.checked) {
     ind.completed = true;
     text.classList.add("completed");
@@ -75,7 +98,6 @@ const countCompletedTasks = () => {
     if (task.completed) ++count;
   });
   completedTaskCounter.innerHTML = `${count}`;
-  console.log(count);
 };
 
 const deleteItems = (arr, id) => {
@@ -85,8 +107,8 @@ const deleteItems = (arr, id) => {
 };
 
 document.addEventListener("click", (e) => {
-  /*   const checkBoxes = document.querySelectorAll(".task-check"); */
   const getId = +e.target.parentElement.getAttribute("id");
+
   if (e.target.classList.contains("del-btn")) {
     deleteItems(tasks, getId);
     countCompletedTasks();
@@ -94,6 +116,57 @@ document.addEventListener("click", (e) => {
   if (e.target.classList.contains("task-check")) {
     changeTaskStatusAndTaskText(tasks, getId, e);
     countCompletedTasks();
-    console.log(tasks);
+  }
+  if (e.target.classList.contains("edit")) {
+    changeToEditionMode(tasks, getId, e);
+    e.target.textContent = "Save";
+    const cancelButton = createElement("button", "cancel-btn");
+    cancelButton.textContent = "Cancel";
+    const editButton = document.querySelectorAll(".edit");
+    const inputText = createElement("input", "new-input");
+    inputText.setAttribute("type", "text");
+    const taskText = document.querySelectorAll(".task");
+
+    editButton.forEach((button) => {
+      if (
+        e.target === button &&
+        e.target.textContent === "Save" &&
+        !button.nextElementSibling.classList.contains("cancel-btn")
+      ) {
+        button.insertAdjacentElement("afterend", cancelButton);
+      }
+    });
+
+    console.log(taskText);
+    taskText.forEach((text) => {
+      if (text.textContent === e.target.previousElementSibling.textContent) {
+        text.replaceWith(inputText);
+      }
+    });
+  }
+  if (e.target.textContent === "Save") {
+    const newInputValue = e.target.previousElementSibling.value;
+    if (newInputValue !== "") {
+      changeText(tasks, getId, newInputValue);
+      renderTaskRow();
+    }
+  }
+  if (e.target.textContent === "Cancel") {
+    changeToEditionMode(tasks, getId, e);
+    renderTaskRow();
   }
 });
+
+function changeText(arr, id, value) {
+  const findInd = arr.find((el) => el.id === id);
+  console.log(findInd);
+  findInd.task = value;
+}
+
+function changeToEditionMode(arr, id, e) {
+  const findInd = arr.find((el) => el.id === id);
+  findInd.isInEditionMode = true;
+  if (e.target.textContent === "Save" || e.target.textContent === "Cancel") {
+    findInd.isInEditionMode = false;
+  }
+}
